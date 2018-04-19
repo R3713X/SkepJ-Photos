3ecce07a1667742f680b70ddc98238fcd148f1ab
piper006 <skyjunior97@hotmail.com>
2018-04-19 17:21:44 +0300

    fixes2

--- src/Model/PhotoController.java
+++ src/Model/PhotoController.java
@@ -2,18 +2,25 @@ package Model;
 
 import Controller.AwtToFx;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class PhotoController {
 
-    public HashMap<String,Image> getAllImages(HashMap<String,byte[]> photoData){
+    public HashMap<String,ImageView> getAllImages(HashMap<String,byte[]> photoData){
 
-        HashMap<String,Image> imageList = new HashMap<>();
+        HashMap<String,ImageView> imageList = new HashMap<>();
+        ImageView[] imageView = new ImageView[200];
 
         AwtToFx awtToFx = new AwtToFx();
+        int i =0;
         for(Map.Entry<String,byte[]> data : photoData.entrySet()) {
-            imageList.put(data.getKey(), awtToFx.getJavaFXImage(data.getValue(), 3000, 3000));
+
+            imageView[i].setImage(awtToFx.getJavaFXImage(data.getValue(), 3000, 3000));
+            imageList.put(data.getKey(),imageView[i]);
+            i++;
         }
         return imageList;
 
